d98182b7fe56bea1eeebee37ffd0d14b401c7d4e
Colin Spangelo <colin.spangelo@gmail.com>
2023-05-05 15:13:15 -0600

    Creating an Age Calculator with parameters age, birth year, current year

---
+++ ageCalculator.js
@@ -0,0 +1,20 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  var age = currentYear-yearOfBirth
+  var name;
+  var yearOfBirth;
+  var currentYear;
+  console.log(name +" is " + age +" years old.")
+return name +" is " + age +" years old."
+}
+ageCalculator("Colin" , 1994, 2023);
+
+
+/*
+This is the code tidied
+
+function ageCalculator(name, yearOfBirth, currentYear){
+  var age = currentYear - yearOfBirth;
+  console.log(name + " is " + age + " years old.");
+  return name + " is " + age + " years old.";
+}
+*/
